7ec0d1c5be10f2a50f21ce57c94c8ae92fd462c2
Andreas Hoerster <andreas-hoerster@web.de>
2016-10-14 19:21:36 +0200

    Added Teil 1.1 with comments, tested and works

--- LBP2.pl
+++ LBP2.pl
@@ -1,1 +1,15 @@
-collect_komp([Komponist|Rest]):- 
+%Teil 1
+
+%Teil 1.1
+
+%Sammelt eine Liste der Komponisten ein die in der DB gegeben sind, anhand von Geburts und Sterbedatum.
+%Findall sammelt anhand eines template Dinge aus dem Goal "komponist" ein indem es dies durchgeht und dann das Resultat mit List
+%unifiziert. Dabei müssen die Bedingungen aus Checklist für die genannten Einträge erfüllt sein.
+%Also ungleich null. Nur wenn dies erfüllt ist, wird der Eintrag Teil der Liste.
+%Am Ende wird die Liste mit dem definierten Inhalt ausgegeben und das Programm evaluiert zu true.
+
+collect_komp(List) :- findall((VORNAME, NAME, GEBOREN, GESTORBEN),			%template
+                      		(komponist(KNR, VORNAME, NAME, GEBOREN, GESTORBEN),		%Goal
+                       		KNR \== null,VORNAME\==null,NAME \== null,GEBOREN\==null,GESTORBEN\==null), %Checklist							
+                      		List).												%unifies the contents of template with List	
+	
