8f4fcc68210c9a9a3120a8001530ecd231fe2b24
Andreas Hoerster <andreas-hoerster@web.de>
2016-10-14 19:45:34 +0200

    even more variable name change!

--- LBP2.pl
+++ LBP2.pl
@@ -5,11 +5,11 @@
 %Sammelt eine Liste der Komponisten ein die in der DB gegeben sind, anhand von Geburts und Sterbedatum.
 %Findall sammelt anhand eines template Dinge aus dem Goal "komponist" ein indem es dies durchgeht und dann das Resultat mit List
 %unifiziert. Dabei müssen die Bedingungen aus Checklist für die genannten Einträge erfüllt sein.
-%Also ungleich null. Nur wenn dies erfüllt ist, wird der Eintrag Teil der Liste.
+%Also ungleich null. Nur wenn dies erfüllt ist, wird der Eintrag Teil der Liste, ansonsten wird er übersprungen.
 %Am Ende wird die Liste mit dem definierten Inhalt ausgegeben und das Programm evaluiert zu true.
 
 collect_komp(List) :- findall((Vorname, Name, Geboren, Gestorben),			%template
-                      		(komponist(NR, Vorname, Name, Geboren, Gestorben),		%Goal
-                       		NR\=null,Vorname\=null,Name\=null,Geboren\=null,Gestorben\=null), %Checklist							
+                      		(komponist(Nr, Vorname, Name, Geboren, Gestorben),		%Goal
+                       		Nr\=null,Vorname\=null,Name\=null,Geboren\=null,Gestorben\=null), %Checklist							
                       		List).												%unifies the contents of template with List	
 	
