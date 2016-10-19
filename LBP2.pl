a31e8c172fca9e13c1ecea57c41626f11de6417f
trashtatur <andreas-hoerster@web.de>
2016-10-19 14:55:04 +0200

    Added Part 1.4 with extensive description of its workings, also modified some comments to better reflect that these are not methods or functions

--- LBP2.pl
+++ LBP2.pl
@@ -3,9 +3,10 @@
 %Teil 1.1
 
 %Sammelt eine Liste der Komponisten ein die in der DB gegeben sind, anhand von Geburts und Sterbedatum.
-%Findall sammelt anhand eines template Dinge aus dem Goal "komponist" ein indem es dies durchgeht und dann das Resultat mit List
-%unifiziert. Dabei mÃ¼ssen die Bedingungen aus Checklist fÃ¼r die genannten EintrÃ¤ge erfÃ¼llt sein.
-%Also ungleich null. Nur wenn dies erfÃ¼llt ist, wird der Eintrag Teil der Liste, ansonsten wird er Ã¼bersprungen.
+%Findall sammelt anhand eines template das alles andere nicht mitnimmt, Dinge aus dem Goal "komponist" 
+%ein indem es dies durchgeht und dann das Resultat mit List
+%unifiziert. Dabei muessen die Bedingungen aus Checklist fuer die genannten EintrÃ¤ge erfuellt sein.
+%Also ungleich null. Nur wenn dies erfuellt ist, wird der Eintrag Teil der Liste, ansonsten wird er uebersprungen.
 %Am Ende wird die Liste mit dem definierten Inhalt ausgegeben und das Programm evaluiert zu true.
 
 collect_komp(List) :- findall((Vorname, Name, Geboren, Gestorben),										%template
@@ -25,11 +26,32 @@ collect_az(List) :- findall((Vorname,Name,Geboren,Gestorben),											%templat
                       	   
 %Teil 1.3
 
-%Arbeitet wie Teil 1 mit dem findall, nur dass hier die member Funktion überprüft ob das Element Hersteller ein Teil der übergebenen Liste HListe ist prüft.
+%Arbeitet wie Teil 1 mit dem findall, nur dass hier das Praedikat member überprüft ob das Element Hersteller ein Teil der uebergebenen Liste HListe ist prüft.
 
 collect_dp(HListe,Collection):- findall((Name,Hersteller),												%template
 								(cd(CDNr, Name, Hersteller, _Anzahl_CDs, _Gesamtspielzeit),				%Goal
 								member(Hersteller,HListe), Name\=null, CDNr\=null),						%Checklist bzw Filter (diesmal mit member als Hilfsfunktion)
-								Collection).                      	   									%unifies contents of template with List after sorting through filter	
+								Collection).                      	   									%unifies contents of template with List after sorting through filter
+								
+%Teil 1.4
+
+%Die Suche wird umgekehrt begonnen und zwar bei den Solisten.
+%hierbei muss die Snr mit der Snr des Stückes unifizieren, 
+%dessen Knr wiederum mit der des Komponisten unifizieren muss
+%somit ist gesichert dass der Komponist an dem Stück beteiligt war,
+%an dem der Komponist beteiligt war
+%findall unifiziert dann alle passenden Komponisten dieser vorangegangenen
+%Bedingung anhand von Vorname und Nachname mit Liste
+%und gibt diese aus.
+									   
+collect_komp(Solist,Liste) :- findall((Vorname, Name),
+                      		  (solist(_CDNr, Snr, Solist, _Instrument), Snr \= null,
+                       		  % Snr aus Praedikat Solist muss mit Snr aus Praedikat stueck unifizierbar sein. Filterbedingung
+                       		  stueck(Snr, Knr, _Titel, _Tonart, _Opus), Knr \= null,
+                       		  % Knr aus Stueck muss mit Knr aus Praedikat komponist unifizierbar sein, Filterbedingung. Anhand
+                       		  %dessen ermittelt findall dann Vorname und Nachname der jeweils passenden Komponisten.
+                       		  komponist(Knr, Vorname, Name, _Geboren, _Gestorben), Name \= null),
+                       		  Liste).								   
+									   									
 
 	
